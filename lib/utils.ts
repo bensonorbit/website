d9878ed838756c551104898660970f988eb7a298
Nick Oates <nick@nickoates.com>
2024-12-15 09:49:53 -0800

    Update meta

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,13 @@
 import { Metadata } from "next";
 
 export function mergeMeta(metadata: Metadata): Metadata {
-	const title =
-		typeof metadata.title === "string" ? metadata.title : "The Benson Orbit";
 	return {
 		...metadata,
 		openGraph: {
 			type: "website",
 			siteName: "The Benson Orbit",
-			title,
-			description:
-				metadata.description ??
-				"The student-run newspaper of Benson Polytechnic High School",
+			title: typeof metadata.title === "string" ? metadata.title : undefined,
+			description: metadata.description || undefined,
 			locale: "en_US",
 			...metadata.openGraph,
 		},
